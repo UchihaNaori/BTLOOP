1c278c54228dc7940188c39b538b57618fd01b4b
hngo512 <hngo419512@gmail.com>
2021-04-18 19:43:27 +0700

    added class MyLine 19h43 18/4

--- src/com/nhom6/MyLine.java
+++ src/com/nhom6/MyLine.java
@@ -1,4 +1,5 @@
 package com.nhom6;
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
@@ -12,15 +13,18 @@ public class MyLine {
 	private Line2D.Double line;
 	// chi so cua duong thang duoc them vao
 	private int indexLine = 0;
-	private final double r = 15;
+	// ban kinh hinh tron cua point
+	private final double radius = 15;
 	public MyLine(Point p1, Point p2, int indexP1, int indexP2) {
 		line = new Line2D.Double(p1, p2);
 		this.indexP1 = indexP1;
 		this.indexP2 = indexP2;
 	}
     // ve duong thang di qua tam 2 point
-	public void drawLine1(Graphics2D g) {
+	public void drawLineMiddle(Graphics2D g) {
 		g.setColor(Color.black);
+		// tang do rong cua line
+		g.setStroke(new BasicStroke(3));
 		g.draw(line);
 		// ve chi so cua duong thang
 		if (indexLine > 0) {
@@ -30,15 +34,17 @@ public class MyLine {
 		
 	}
 	// ve duong thang ben trai qua 2 point khi giua hai diem co 2 duong thang
-	public void drawLine2(Graphics2D g) {
+	public void drawLineLeft(Graphics2D g) {
 		g.setColor(Color.black);
+		// tang do rong cua line
+		g.setStroke(new BasicStroke(3) );
 		// do dai duong thang qua 2 diem 
-		double d = Math.sqrt(Math.pow((line.x2-line.x1), 2)+ Math.pow((line.y2-line.y1), 2));
+		double range = Math.sqrt(Math.pow((line.x2 - line.x1), 2) + Math.pow((line.y2 - line.y1), 2) );
 		// tao 2 diem va duong thang di qua 2 diem ben trai (bien doi toan hoc)
-		Point p1 = new Point((int)(line.x1+(line.y2-line.y1)*r/d),(int)(line.y1+(line.x1-line.x2)*r/d));
-		Point p2 = new Point((int)(line.x2+(line.y2-line.y1)*r/d),(int)(line.y2+(line.x1-line.x2)*r/d));
-		line = new Line2D.Double(p1,p2);
-		g.draw(line);
+		Point p1 = new Point((int) (line.x1 + (line.y2 - line.y1) * radius / range), (int) (line.y1 + (line.x1 - line.x2) * radius / range) );
+		Point p2 = new Point((int) (line.x2 + (line.y2 - line.y1) * radius / range), (int) (line.y2 + (line.x1 - line.x2) * radius / range) );
+		Line2D.Double line1 = new Line2D.Double(p1, p2);
+		g.draw(line1);
 		// ve chi so cua duong thang
 		if (indexLine > 0) {
 			String str = "(" + indexLine + ")";
@@ -46,14 +52,16 @@ public class MyLine {
 		}
 	}
 	// ve duong thang ben phai qua 2 point khi giua hai diem co 2 duong thang
-	public void drawLine3(Graphics2D g) {
+	public void drawLineRight(Graphics2D g) {
 		g.setColor(Color.black);
+		// tang do rong cua line
+		g.setStroke(new BasicStroke(3));
 		// tao 2 diem va duong thang di qua 2 diem ben phai (bien doi toan hoc)
-		double d = Math.sqrt(Math.pow((line.x2-line.x1), 2)+ Math.pow((line.y2-line.y1), 2));
-		Point p1 = new Point((int)(line.x1+(line.y1-line.y2)*r/d),(int)(line.y1+(line.x2-line.x1)*r/d));
-		Point p2 = new Point((int)(line.x2+(line.y1-line.y2)*r/d),(int)(line.y2+(line.x2-line.x1)*r/d));
-		line = new Line2D.Double(p1,p2);
-		g.draw(line);
+		double range = Math.sqrt(Math.pow((line.x2 - line.x1), 2) + Math.pow((line.y2 - line.y1), 2) );
+		Point p1 = new Point((int) (line.x1 + (line.y1-line.y2) * radius / range),(int) (line.y1 + (line.x2 - line.x1) * radius / range) );
+		Point p2 = new Point((int) (line.x2 + (line.y1-line.y2) * radius / range),(int) (line.y2 + (line.x2 - line.x1) * radius / range) );
+		Line2D.Double line1 = new Line2D.Double(p1, p2);
+		g.draw(line1);
 		// ve chi so cua duong thang
 		if (indexLine > 0) {
 			String str = "(" + indexLine + ")";
@@ -61,30 +69,36 @@ public class MyLine {
 		}
 	}
 	// ve mui ten cho truong hop drawLine1
-	public void drawArrow1(Graphics2D g) {
+	public void drawArrowMiddle(Graphics2D g) {
 		g.setColor(Color.black);
 		// tim toa do 3 diem tạo thanh tam giac mui ten
-		double d = Math.sqrt(Math.pow((line.x2-line.x1), 2)+ Math.pow((line.y2-line.y1), 2));
-		Line2D.Double line1= new Line2D.Double(line.x1+r*(line.x2-line.x1)/d, line.y1+r*(line.y2-line.y1)/d, line.x2-r*(line.x2-line.x1)/d, line.y2-r*(line.y2-line.y1)/d);
-		double d1 = Math.sqrt(Math.pow((line1.x2-line1.x1), 2)+ Math.pow((line1.y2-line1.y1), 2));
-		int[] x = {(int)line1.x2, (int)(line1.x2+10*(line1.x1-line1.x2)/d1+4*(line1.y2-line.y1)/d1), (int)(line1.x2+10*(line1.x1-line1.x2)/d1-4*(line1.y2-line1.y1)/d1) };
-		int[] y = {(int)line1.y2, (int)(line1.y2+10*(line1.y1-line1.y2)/d1+4*(line1.x1-line.x2)/d1), (int)(line1.y2+10*(line1.y1-line1.y2)/d1-4*(line1.x1-line1.x2)/d1) };
+		// range la khoang cach 2 point
+		double range = Math.sqrt(Math.pow((line.x2 - line.x1), 2) + Math.pow((line.y2 - line.y1), 2) );
+		// line1 la duong noi ngan nhat giua 2 diem nam tren hai duong tron co tam la 2 point
+		Line2D.Double line1= new Line2D.Double(line.x1 + radius * (line.x2 - line.x1) / range, line.y1 + radius * (line.y2 - line.y1) / range, line.x2 - radius * (line.x2 -line.x1) / range, line.y2 - radius * (line.y2 - line.y1) / range);
+		// range1 la chieu dai cua line1
+		double range1 = Math.sqrt(Math.pow((line1.x2 - line1.x1), 2) + Math.pow((line1.y2 - line1.y1), 2) );
+		// xPoint, yPoint chua toa do 3 diem tao thanh tam giac mui ten
+		int[] xPoint = {(int) line1.x2, (int) (line1.x2 + 10 * (line1.x1 - line1.x2) / range1 + 4 * (line1.y2 - line.y1) / range1), (int) (line1.x2 + 10 * (line1.x1 - line1.x2) / range1 - 4 * (line1.y2 - line1.y1) / range1) };
+		int[] yPoint = {(int) line1.y2, (int) (line1.y2 + 10 * (line1.y1 - line1.y2) / range1 + 4 * (line1.x1 - line.x2) / range1), (int) (line1.y2 + 10 * (line1.y1 - line1.y2) / range1 - 4 * (line1.x1 - line1.x2) / range1) };
 		// ve mui ten
-		Polygon p= new Polygon(x, y, 3);
+		Polygon p = new Polygon(xPoint, yPoint, 3);
 		g.fillPolygon(p);
-		g.drawPolygon(x, y, 3);
+		g.drawPolygon(xPoint, yPoint, 3);
 	}
 	// ve mui ten cho truong hop drawLine2 va drawLine3
-	public void drawArrow2(Graphics2D g) {
+	public void drawArrowLeftRight(Graphics2D g) {
 		g.setColor(Color.black);
 		// tim qua do 3 diem tao thanh tam giac mui ten 
-		double d = Math.sqrt(Math.pow((line.x2-line.x1), 2)+ Math.pow((line.y2-line.y1), 2));
-		int[] x = {(int)line.x2, (int)(line.x2+10*(line.x1-line.x2)/d+4*(line.y2-line.y1)/d), (int)(line.x2+10*(line.x1-line.x2)/d-4*(line.y2-line.y1)/d) };
-		int[] y = {(int)line.y2, (int)(line.y2+10*(line.y1-line.y2)/d+4*(line.x1-line.x2)/d), (int)(line.y2+10*(line.y1-line.y2)/d-4*(line.x1-line.x2)/d) };
+		// range la khoang cach 2 point
+		double range = Math.sqrt(Math.pow((line.x2 - line.x1), 2) + Math.pow((line.y2 - line.y1), 2) );
+		// xPoint, yPoint chua toa do 3 diem tao thanh tam giac mui ten
+		int[] xPoint = {(int) line.x2, (int) (line.x2 + 10 * (line.x1 - line.x2) / range + 4 * (line.y2 - line.y1) / range), (int) (line.x2 + 10 * (line.x1 - line.x2) / range - 4 * (line.y2 - line.y1) / range) };
+		int[] yPoint = {(int) line.y2, (int) (line.y2 + 10 * (line.y1 - line.y2) / range + 4 * (line.x1 - line.x2) / range), (int) (line.y2 + 10 * (line.y1 - line.y2) / range - 4 * (line.x1 - line.x2) / range) };
 		// ve mui ten
-		Polygon p= new Polygon(x, y, 3);
+		Polygon p = new Polygon(xPoint, yPoint, 3);
 		g.fillPolygon(p);
-		g.drawPolygon(x, y, 3);
+		g.drawPolygon(xPoint, yPoint, 3);
 	}
 
 	public int getIndexP1() {
